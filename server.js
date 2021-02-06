930d986cf072ad6f1b0cc00a0f109c2eb591fa96
Armando Xhimanki <armando.xhimanki@gmail.com>
2021-02-06 05:29:45 -0500

    heroku:

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(session({
-    secret: 'SEIRFLEXRocks!',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true
   }));
